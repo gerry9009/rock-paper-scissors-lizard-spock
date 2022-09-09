fc95aa9b18a6c37dcddb700d6f2f5e167a2215b6
Buglyo Gergo <buglyogergo@Buglyo-MacBook-Pro.local>
2022-09-09 21:53:01 +0200

    Fixed mispelling in main page

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 ## Rock, Paper, Scissors, Lizard, Spock
 
-## Rules: 
-* Scissors cuts Paper
-* Paper covers Rock
-* Rock crushes Lizard
-* Lizard poisons Spock
-* Spock smashes Scissors
-* Scissors decapitates Lizard
-* Lizard eats Paper
-* Paper disproves Spock
-* Spock vaporizes Rock
-* Rock crushes Scissors
+## Rules:
+
+- Scissors cuts Paper
+- Paper covers Rock
+- Rock crushes Lizard
+- Lizard poisons Spock
+- Spock smashes Scissors
+- Scissors decapitates Lizard
+- Lizard eats Paper
+- Paper disproves Spock
+- Spock vaporizes Rock
+- Rock crushes Scissors
 
 ## **The game published in [here](https://gerry9009.github.io/rock-paper-scissors-lizard-spock/)**
-
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import "./Footer.css";
-import rules from '../../img/rules.png';
-
-//TODO make a popup window where will show the rules
+import rules from "../../img/rules.png";
 
 export default class Footer extends React.Component {
-    handlePopup() {
-        document.querySelector(".js-popup").classList.toggle("footer-popup-hidden");
-    }
+  handlePopup() {
+    document.querySelector(".js-popup").classList.toggle("footer-popup-hidden");
+  }
 
-    render() {
-        return (
-        <footer>
-            <button className="footer-btn" onClick={this.handlePopup}>Rules</button>
-            <div className="footer-popup footer-popup-hidden js-popup" onClick={this.handlePopup}>
-                <div className="popup-container">
-                    <img className="popup-img" src={rules} alt="rules" onClick={this.handlePopup}/>
-                    <div className="popup-exit"></div>
-                </div>
-            </div>
-        </footer>
-        );
-    }
+  render() {
+    return (
+      <footer>
+        <button className="footer-btn" onClick={this.handlePopup}>
+          Rules
+        </button>
+        <div
+          className="footer-popup footer-popup-hidden js-popup"
+          onClick={this.handlePopup}
+        >
+          <div className="popup-container">
+            <img
+              className="popup-img"
+              src={rules}
+              alt="rules"
+              onClick={this.handlePopup}
+            />
+            <div className="popup-exit"></div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
